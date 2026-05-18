1dbc84483f89582345e487e2628290e50efd0d6c
--local <gritsruslan@gmail.com>
2026-05-18 15:12:21 +0300

    get all categories use case implementation

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,6 +1,12 @@
-// TODO: Логіка для роботи з категоріями
 // CRUD операції та бізнес логіка
+const Category = require("../models/category.js");
+
+// Отримання всіх категорій
+exports.getCategories = async (req, res) => {
+    const categories = await Category.getAll();
+    return res.status(200).json(categories);
+};
 
 module.exports = {
-    // Експорт функцій контролера
+    getCategories,
 };
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// TODO: Імпортувати контролери
+
+const categoriesController = require("../controllers/categories");
+const asyncHandler = require("../middleware/asyncHandler");
 
 // A1 - Перегляд категорій
 // GET /api/categories
+router.get("/", asyncHandler(categoriesController.getCategories));
 
 module.exports = router;
